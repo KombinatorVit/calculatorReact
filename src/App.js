31398fe058308d92a47b7ba6db9b000503afaa00
KombinatorVit <slonik0775@gmail.com>
2023-02-11 22:13:27 +0200

    меню для выбора типа интерфейса

--- src/App.js
+++ src/App.js
@@ -1,31 +1,43 @@
 import './App.css';
-import { HamburgerIcon } from '@chakra-ui/icons'
+import {HamburgerIcon} from '@chakra-ui/icons'
+import {List, ListItem, UnorderedList} from '@chakra-ui/react'
+import {SlideFade, useDisclosure} from '@chakra-ui/react'
 
 import Calculator from "./components/Calculator";
 import {Box} from "@chakra-ui/react";
 
 
+function Menu() {
+    const {isOpen, onToggle} = useDisclosure()
 
-function Menu(){
     return (
-        <Box>
+<Box>
+    <HamburgerIcon w={'45px'} h={'45px'} p={'5px'} m={'5px'} borderRadius={'5px'} onClick={onToggle}/>
 
+    <SlideFade in={isOpen} offsetY={'-20px'} unmountOnExit>
+
+        <Box bg={'gray.100'} p={'10px'} m={'4px'} borderRadius={'8px'} w={'60%'} position={'absolute'} zIndex={'10'}>
+            <List display={'flex'} flexDirection={'column'} gap={'10px'} fontSize={'20px'}>
+                <ListItem>Calculator</ListItem>
+                <ListItem>Converter</ListItem>
+                <ListItem>Settings</ListItem>
+            </List>
         </Box>
+    </SlideFade>
+</Box>
     )
 }
 
 function App() {
 
 
-
     return (
 
-<Box h={'90vh'}>
-    <HamburgerIcon w={'45px'} h={'45px'} p={'5px'} m={'5px'} borderRadius={'5px'} />
-    <Calculator />
+        <Box h={'90vh'}>
+            <Menu />
+            <Calculator/>
 
-</Box>
-
+        </Box>
 
 
     );
