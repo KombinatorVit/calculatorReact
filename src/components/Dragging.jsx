f17d3b42359e50d5347910f51d2ce76d0351f52e
KombinatorVit <slonik0775@gmail.com>
2023-02-12 10:55:14 +0200

    Drag&Drop update

--- src/components/Dragging.jsx
+++ src/components/Dragging.jsx
@@ -12,6 +12,7 @@ const Dragging = (props) => {
 
     })
 
+
     function drag(element) {
         let draggable = element.target
         draggable.onmousedown = function (event) {
@@ -20,63 +21,67 @@ const Dragging = (props) => {
             document.body.append(draggable)
             moveAt(event.pageX, event.pageY)
 
-            function moveAt(pageX, pageY) {
-                draggable.style.left = pageX - draggable.offsetWidth / 2 + 'px'
-                draggable.style.top = pageY - draggable.offsetHeight / 2 + 'px'
+        }
+
+        function moveAt(pageX, pageY) {
+            draggable.style.left = pageX - draggable.offsetWidth / 2 + 'px'
+            draggable.style.top = pageY - draggable.offsetHeight / 2 + 'px'
+        }
+
+
+        let currentDroppable = null
+
+        function onMouseMove(event) {
+            moveAt(event.pageX, event.pageY)
+            draggable.hidden = true
+            let elemBelow = document.elementFromPoint(event.clientX, event.clientY)
+            draggable.hidden = false
+            if (!elemBelow) return
+            let droppableBelow = elemBelow.closest('.droppable')
+            if (currentDroppable != droppableBelow) {
+                if (currentDroppable) {
+                    leaveDroppable(currentDroppable)
+                }
+                currentDroppable = droppableBelow
+                if (currentDroppable) {
+                    enterDroppable(currentDroppable)
+                }
+
+
             }
-
-            let currentDroppable = null
-
-            function onMouseMove(event) {
-                moveAt(event.pageX, event.pageY)
-                draggable.hidden = true
-                let elemBelow = document.elementFromPoint(event.clientX, event.clientY)
-                draggable.hidden = false
-                if (!elemBelow) return
-                let droppableBelow = elemBelow.closest('.droppable')
-                if (currentDroppable != droppableBelow) {
-                    if (currentDroppable) {
-                        leaveDroppable(currentDroppable)
-                    }
-                    currentDroppable = droppableBelow
-                    if (currentDroppable) {
-                        enterDroppable(currentDroppable)
-                    }
+        }
 
 
-                }
-
-                function enterDroppable(elem) {
-                    draggable.onmouseup = function () {
-                        draggable.style.display = 'none'
-                        props.setResult(eval(`${props.result}
-                        ${elem.innerHTML}
-                        ${draggable.innerHTML}`))
-                        document.removeEventListener('mouseove', onMouseMove);
-                        draggable.onmouseup = null;
-                        draggable = null;
-                        return
-                    }
-                    draggable.style.cursor = 'copy'
-
-                }
-
-
-                function leaveDroppable(elem) {
-                    elem.style.background = ''
-                    draggable.style.cursor = 'default'
-
-                }
-            }
-            document.addEventListener('mousemove', onMouseMove);
-            draggable.onmouseup = function() {
+        function enterDroppable(elem) {
+            draggable.onmouseup = function () {
                 draggable.style.display = 'none'
-document.removeEventListener('mousemove', onMouseMove)
-                props.setHistory(draggable.innerHTML)
-                draggable.onmouseup = null
-                draggable = null
-
+                props.setResult(eval(`${props.result}
+                ${elem.innerHTML}
+                ${draggable.innerHTML}`))
+                document.removeEventListener('mousemove', onMouseMove);
+                draggable.onmouseup = null;
+                draggable = null;
+                return
             }
+            draggable.style.cursor = 'copy'
+
+        }
+
+
+        function leaveDroppable(elem) {
+            elem.style.background = ''
+            draggable.style.cursor = 'default'
+
+        }
+
+        document.addEventListener('mousemove', onMouseMove);
+        draggable.onmouseup = function () {
+            draggable.style.display = 'none'
+            document.removeEventListener('mousemove', onMouseMove)
+            props.setHistory(draggable.innerHTML)
+            draggable.onmouseup = null
+            draggable = null
+
         }
     }
 
